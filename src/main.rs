87d338f09ddac5b7a143d1cdfcd0a7f80cea5179
Sivaram Sajeev <sivaramsajeev@gmail.com>
2022-07-08 14:37:38 +0530

    file_name added

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-
 use std::collections::HashMap;
 use std::io::Read;
 use std::str::FromStr;
 
 fn main() {
+    let file_name = "/tmp/db.txt";
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    let mut todo = Todo::new().expect("Initialisation of db failed");
+    let mut todo = Todo::new(file_name.to_string())
+                    .expect("Initialisation of db failed");
 
     if action == "add" {
         todo.insert(item);
@@ -15,20 +16,29 @@ fn main() {
             Ok(_) => println!("todo saved"),
             Err(why) => println!("An error occurred: {}", why),
         }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => match todo.save() {
+                Ok(_) => println!("todo saved"),
+                Err(why) => println!("An error occurred: {}", why),
+            },
+        }
     };
 }
 
 struct Todo {
+    file_name: String,
     map: HashMap<String, bool>,
 }
 
 impl Todo {
-    fn new() -> Result<Todo, std::io::Error> {
+    fn new(file_name: String) -> Result<Todo, std::io::Error> {
         let mut f = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
-            .open("/tmp/db.txt")?;
+            .open(&file_name)?;
         let mut content = String::new();
         f.read_to_string(&mut content)?;
         let map: HashMap<String, bool> = content
@@ -39,7 +49,10 @@ impl Todo {
             .map(|(k, v)| (String::from(k), bool::from_str(v)
             .unwrap()))
             .collect();
-        Ok(Todo { map })
+        Ok(Todo { 
+            file_name: file_name,
+            map: map,
+         })
     }
 
     fn insert(&mut self, key: String) {
@@ -52,7 +65,13 @@ impl Todo {
             let record = format!("{}\t{}\n", k, v);
             content.push_str(&record)
         }
-        std::fs::write("/tmp/db.txt", content)
+        std::fs::write(self.file_name, content)
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
     }
 }
-
