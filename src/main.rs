b344136722db17913a2532c6b0afe16875fcd906
Sivaram Sajeev <sivaramsajeev@gmail.com>
2022-07-08 12:46:50 +0530

    collected

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+
 use std::collections::HashMap;
+use std::io::Read;
+use std::str::FromStr;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
     if action == "add" {
         todo.insert(item);
         match todo.save() {
@@ -21,6 +23,23 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("/tmp/db.txt")?;
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
@@ -34,3 +53,4 @@ impl Todo {
         std::fs::write("/tmp/db.txt", content)
     }
 }
+
